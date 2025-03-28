02c48c1bdc4d27a1aca431325830afb4dc956396
karasi-sonica <karasisonica@gmail.com>
2025-03-28 11:38:11 +0530

    get_api finished

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,32 +5,6 @@ const User = require("../model/User");
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-    const { email, username,password } = req.body;
-    
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User ({ email, username, password: hashedPassword });
-
-    try{
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully!" });
-    }catch(error){
-        res.status(200).json({ error: "Error registering User :( " });
-    }
-});
-
-router.post("/login", async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({ username });
-
-    if(!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    const token = jwt.sign({ userId: user._id }, "secret_key", { expiresIn: "1h" });
-    res.json({ token, message: "Login Successful" });
-});
-
 router.get("/users", async (req, res) => {
     const users = await User.find();
     res.json(users);
